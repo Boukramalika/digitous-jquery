5764487e021f87378b7eab765880c342001904dc
boukramalika <bouhedjarmalika92@gmail.com>
2021-05-07 10:17:30 +0200

    exercice 29

--- exercice29/js/main.js
+++ exercice29/js/main.js
@@ -3,8 +3,10 @@ $(function () {
         let test= $("input").val()
         if (test.length > 5) {  
             $("input").addClass("is-valid"); 
+            $("input").removeClass("is-invalid"); 
           } else {  
             $("input").addClass("is-invalid");  
+            $("input").removeClass("is-valid"); 
         };
     })  
     })
