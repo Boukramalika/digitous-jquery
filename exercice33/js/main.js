a63ada54992298f9bac67b4755a96043e5018ecb
boukramalika <bouhedjarmalika92@gmail.com>
2021-05-10 14:28:02 +0200

    correction Exercice 33

--- exercice33/js/main.js
+++ exercice33/js/main.js
@@ -1,13 +1,12 @@
 $(document).ready( function (){
     $("button").click(function(){
         $.ajax({
+           
             url: 'https://restcountries.eu/rest/v2/name/france',
             success: function(data, status, response) {
-                $("#exercise").html(
-                    "Mon pays est <strong class='important'>" + data[0].name + "</strong><br>" +
-                    "Mon capital est <strong class='important'>" + data[0].capital
-                    );
-             }
+                $("#country").html( data[0].name )
+                $("#capital").html( data[0].capital )   
+            }     
         })  
     });
 })
